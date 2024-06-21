7095bfeb073ab8f840ec65ba86afc25685436190
Miguel <micmigm@gmail.com>
2024-06-21 19:55:25 -0300

    Agregado efecto al botón copiar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     const textarea = document.getElementById('texto-in');
-
+    const btnCopiar = document.getElementById('btn-copiar');
+    
     // Redimensiona el textarea texto-in a la cantidad de caracteres que contiene
     textarea.addEventListener('input', function() {
         this.style.height = 'auto'; 
-        this.style.height = this.scrollHeight + 'px'; 
+        this.style.height = this.scrollHeight + 'px';
+
+        btnCopiar.classList.remove('boton-copiar-clickeado');
+    });
+
+    // Cambia el color del botón Copiar cuando se hace click
+    // Se reinicia cuando se modifica el contenido de texto-in
+    btnCopiar.addEventListener('click', function() {
+        btnCopiar.classList.add('boton-copiar-clickeado');
     });
 });
