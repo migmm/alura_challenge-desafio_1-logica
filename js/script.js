34b4049c4e3f142978743893677f16a242dedcbb
Miguel <micmigm@gmail.com>
2024-06-22 00:42:13 -0300

    Agregado de la funcion desencriptar

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const textoIn = document.getElementById('texto-in');
     const textoOut = document.getElementById('texto-out');
     const btnEncriptar = document.getElementById('btn-encriptar');
+    const btnDesencriptar = document.getElementById('btn-desencriptar');
     const btnCopiar = document.getElementById('btn-copiar');
     const mensajeError = document.querySelector('.mensaje-error');
 
@@ -15,6 +16,18 @@ document.addEventListener('DOMContentLoaded', function() {
             'u': 'ufat'
         };
         return texto.replace(/[aeiou]/g, letra => mapeo[letra]);
+    }
+
+    // Función para desencriptar dados los requerimientos.
+    function desencriptar(texto) {
+        const mapeo = {
+            'ai': 'a',
+            'enter': 'e',
+            'imes': 'i',
+            'ober': 'o',
+            'ufat': 'u'
+        };
+        return texto.replace(/ai|enter|imes|ober|ufat/g, palabra => mapeo[palabra]);
     }
 
     // Función para validar y convertir el texto a minúsculas
@@ -31,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Listener del boton Encriptar que llama a la función validarYConvertir
-    // Si la conversión y la verificación está bien, encripta el texto 
+    // Si la conversión y la verificación está bien, encripta el texto mediante la función correspondiente
     // Y llama a la función mostrarResultado
     btnEncriptar.addEventListener('click', function() {
         const textoValidado = validarYConvertir(textoIn.value);
@@ -41,12 +54,22 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Función que se encarga de mostrar el resultado encriptado
+    // Listener del botón Desencriptar que llama a la función validarYConvertir
+    // Si la conversión y la verificación está bien, desencripta el texto mediante la función correspondiente
+    // Y llama a la función mostrarResultado
+    btnDesencriptar.addEventListener('click', function() {
+        const textoValidado = validarYConvertir(textoIn.value);
+        if (textoValidado) {
+            const textoDesencriptado = desencriptar(textoValidado);
+            mostrarResultado(textoDesencriptado);
+        }
+    });
+
+    // Función que se encarga de mostrar el resultado (encriptado o desencriptado)
     // Y oculta el texto de error, y muestra el textarea con el resultado
     // Y el botón para copiar el texto al portapapeles 
     function mostrarResultado(texto) {
-        const textoEncriptado = encriptar(texto);
-        textoOut.value = textoEncriptado;
+        textoOut.value = texto;
         ajustarAltura(textoOut);
         textoOut.style.display = 'block';
         btnCopiar.style.display = 'block';
